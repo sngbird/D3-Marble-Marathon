a277150ce8bc0f620649aa5b5c5bf0af829f8bbe
Lumina <Luminadang@gmail.com>
2023-05-11 11:42:37 -0700

    Added assets to scene and fixed sprites

--- game.js
+++ game.js
@@ -1,3 +1,29 @@
+class Test extends Phaser.Scene {
+    constructor() {
+        super('test');
+    }
+    preload(){
+        this.load.image('playermarble', "assets/marble.png");
+        this.load.image('platform', "assets/platformtrim.png");
+
+
+    }
+    create() {
+        this.w = this.game.config.width;
+        this.h = this.game.config.height;
+        this.s = this.game.config.width * 0.01;
+        this.cameras.main.setBackgroundColor('#444');
+        // let ground = this.matter.add.rectangle(this.w/2, this.h-100, this.w, 180, { isStatic: true });       
+        // ground.setColor = "#9ff07a";
+        //this.matter.add.image(this.w/2.0, this.h/2, 'platform');
+        let player = this.matter.add.sprite(500,50,'playermarble');
+        player.setScale(.05);
+       let ground = this.matter.add.image(500, 500, 'platform', null, { isStatic: true });
+        ground.Height = 50;
+    
+    }
+}
+
 
 
 function loadFont(name, url) {
@@ -17,13 +43,19 @@ const game = new Phaser.Game({
         height: 1080
     },
     physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 }
+        default: 'matter',
+        matter: {
+            enableSleeping: true,
+            gravity: {
+                y: 1
+            },
+            debug: {
+                showBody: true,
+                showStaticBody: true
+            }
         }
     },
-    //scene: [Credits],
-    scene: [Title, Logo, Bedroom, SpookyPath, MurkyPond, WitchesHut, Cemetary, NormalEnd, GoodEnd, Credits],
+    scene: [Test],
 
-    title: "Spooky Woods",
+    title: "Marble Marathon",
 });
